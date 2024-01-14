2fcdbc433ca8115d122ee237a2a1cd0e2da62b18
luan torres <luan.torres@sensedata.com.br>
2024-01-14 12:14:57 -0300

    wip

--- initial_code.py
+++ initial_code.py
@@ -7,16 +7,32 @@ import cv2 as cv
 
 
 # Reading videos
-capture = cv.VideoCapture('videos/animation.mp4')
+# capture = cv.VideoCapture('videos/animation.mp4')
+# 
+# while True:
+#     isTrue, frame=capture.read()
+#     cv.imshow('Video', frame)
+# 
+#     if cv.waitKey(20) and 0xFF == ord('d'):
+#         break
+# 
+# capture.release()
 
-while True:
-    isTrue, frame=capture.read()
-    cv.imshow('Video', frame)
 
-    if cv.waitKey(20) and 0xFF == ord('d'):
-        break
 
-capture.release()
-cv.destroyAllWindows()
+# Resizing an image
+img = cv.imread('images/snake.jpg', cv.IMREAD_UNCHANGED)
+print('Original Dimensions:', img.shape)
 
+scale_percent = 60
+
+width = int(img.shape[1] * scale_percent / 100)
+heigth = int(img.shape[0] * scale_percent / 100)
+dimension = (width, heigth)
+
+img_resized = cv.resize(img, dimension, interpolation=cv.INTER_AREA)
+print('Resized dimensions: ', img_resized.shape)
+
+cv.imshow('Resized image', img_resized)
 cv.waitKey(0)
+cv.destroyAllWindows()
