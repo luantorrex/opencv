49175926a4a7e64af1175f96f2665450b96acdc9
luan torres <luan.torres@sensedata.com.br>
2024-01-14 11:59:42 -0300

    wip

--- initial_code.py
+++ initial_code.py
@@ -1,7 +1,22 @@
 import cv2 as cv
 
-img = cv.imread('images/snake.jpg', cv.IMREAD_GRAYSCALE)
+# Reading images
+# img = cv.imread('images/snake.jpg', cv.IMREAD_GRAYSCALE)
+# cv.imshow('snake', img)
 
-cv.imshow('snake', img)
+
+
+# Reading videos
+capture = cv.VideoCapture('videos/animation.mp4')
+
+while True:
+    isTrue, frame=capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) and 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
 
 cv.waitKey(0)
